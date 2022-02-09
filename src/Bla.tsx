212b5db0f60a2f404767ba2449e3851a271c78f2
adinu <adisod@gmail.com>
2022-02-09 14:34:09 +0200

    Initial commit

---
+++ src/Bla.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { BarChart } from "recharts";
+
+// implementation
+export interface ButtonProps extends React.ComponentProps<typeof BarChart> {
+  specialProp?: string;
+}
+export function Button(props: ButtonProps) {
+  const { specialProp, ...rest } = props;
+  console.log("rest", rest);
+  // do something with specialProp
+  return <button {...rest} />;
+}
+
+// usage
+function Bla() {
+  // Type '"foo"' is not assignable to type '"button" | "submit" | "reset" | undefined'.(2322)
+  // return <Button type="foo"> sldkj </Button>
+
+  // no error
+  return <Button> text </Button>;
+}
